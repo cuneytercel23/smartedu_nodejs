694adf9a6ceed38de1ecdc64817aea25ad51c9bf
Cüneyt Erçel <cuneyt_9999@hotmail.com>
2022-11-23 11:41:04 +0300

    partials

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express from "express";
 
 const app = express();
+//template engine 
+app.set("view engine", "ejs") // view enginim ejs dedik.
+
+//middlewares
+app.use(express.static("public")) // statik dosyalarım public içinde olucak.
+
 const port = 3000; 
 
 app.get("/", (req,res)=> {
-    res.send("mainpage");
+    res.status(200).render("index"); //index.ejsyi render etmek.
 })
 
+app.get("/about", (req,res)=> {
+    res.status(200).render("about"); //about.ejsyi render etmek.
+})
 app.listen(port, () => {
     console.log(`app started on port ${port}`);
 })
